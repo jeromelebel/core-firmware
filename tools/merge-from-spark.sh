e68f6f3e5572a4ff2671c18500f0f488c3498fc0
Jérôme Lebel <jerome@fotonauts.com>
2014-07-07 01:54:58 +0200

    don't try to merge bin

--- tools/merge-from-spark.sh
+++ tools/merge-from-spark.sh
@@ -16,4 +16,8 @@ git fetch
 git pull
 git checkout master
 git merge spark-master
+git rm build/core-firmware.bin
+git rm build/core-firmware.elf
+git rm build/core-firmware.hex
+git commit
 git push
